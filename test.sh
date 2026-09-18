716467d38e2a9495bc7f4f5fbc75cac76f5a7638
Kevin Campuzano <kevincamp_90@me.com>
2026-09-18 12:43:52 -0500

    Add proper test.sh with modes and output_path

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-npm test -- test/backend-test/test-prometheus.js
+set -e
+
+MODE=${1:-base}
+OUTPUT_PATH=${2:-/tmp/test-output.xml}
+
+npm test -- test/backend-test/test-prometheus_7a3996.js 2>&1 | tee "$OUTPUT_PATH"
